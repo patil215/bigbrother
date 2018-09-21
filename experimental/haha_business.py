6402e027849eed8d4678b4c81230499a2fda403a
Brian Cui <brian.cui@live.com>
2018-09-20 21:23:58 -0700

    Rename haha_business to hello

--- experimental/haha_business.py
+++
@@ -1,5 +0,0 @@
-import cv2
-img = cv2.imread('./haha_business.jpg')
-cv2.imshow('Haha, Business!', img)
-cv2.waitKey()
-
---
+++ experimental/hello.py
@@ -0,0 +1,5 @@
+import cv2
+img = cv2.imread('./hello.jpg')
+cv2.imshow('Hello, World', img)
+cv2.waitKey()
+
