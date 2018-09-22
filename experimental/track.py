134c92bdc0947d2f8a411f4c689fbe735c975a0c
Neil Patil <neilpatil215@gmail.com>
2018-09-22 18:52:30 -0500

    Add tracking script

---
+++ experimental/track.py
@@ -0,0 +1,112 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+import sys
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="Path to video file")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
+
+# 111, 23, 22
+greenLower = (30, 16, 39)
+greenUpper = (85, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+if not args.get("video", False):
+	print("Please supply video file")
+	sys.exit(0)
+else:
+	vs = cv2.VideoCapture(args["video"])
+
+time.sleep(2.0)
+
+while True:
+	frame = vs.read()[1]
+
+	if frame is None:
+		break
+
+	frame = imutils.resize(frame, width=600)
+	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	center = None
+
+	"""
+	if len(cnts) > 0:
+		print(len(cnts))
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 1:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	"""
+	for cnt in cnts:
+		print(len(cnts))
+		#c = max(cnts, key=cv2.contourArea)
+		c = cnt
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 1:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	
+ 
+	# update the points queue
+	pts.appendleft(center)
+
+	# loop over the set of tracked points
+	for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+ 
+	# show the frame to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+ 
+# if we are not using a video file, stop the camera video stream
+if not args.get("video", False):
+	vs.stop()
+ 
+# otherwise, release the camera
+else:
+	vs.release()
+ 
+# close all windows
+cv2.destroyAllWindows()
+
