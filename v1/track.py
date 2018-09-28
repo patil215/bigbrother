4481b472011b2391f5976d3dfb56b4dd4eff50d2
Brian Cui <brian.cui@live.com>
2018-09-28 16:58:38 -0700

    improve v1

--- v1/track.py
+++ v1/track.py
@@ -28,6 +28,21 @@ else:
 
 time.sleep(2.0)
 
+import cv2
+import numpy as np
+
+def create_blank(width, height, rgb_color=(0, 0, 0)):
+        """Create new image(numpy array) filled with certain color in RGB"""
+        # Create black blank image
+        image = np.zeros((height, width, 3), np.uint8)
+
+        # Since OpenCV uses BGR, convert the color first
+        color = tuple(reversed(rgb_color))
+        # Fill image with color
+        image[:] = color
+
+        return image
+
 def findColor(frame, min_bound, max_bound):
         BLUR_RADIUS = 11
         blurred = cv2.GaussianBlur(frame, (BLUR_RADIUS, BLUR_RADIUS), 0)
@@ -53,20 +68,53 @@ def drawColorLocations(frame, cnts):
                 # then update the list of tracked points
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-def drawPath(frame, ps):
+def drawPath(frameName, ps):
         pts = []
         for i in range(len(ps)):
                 pt = ps[i]
-                pts += [(int(pt[0] * 3.0) - 1500,
-                         int(pt[1] * 3.0) - 1500 - int(i / 4) * 1)]
-                
+                pts += [[int(pt[0] * 10.0), int(pt[1] * 10.0)]]
+
+        min_x = float('inf')
+        min_y = float('inf')
+        max_x = float('-inf')
+        max_y = float('-inf')
+
+        for pt in pts:
+                if pt[0] < min_x:
+                        min_x = pt[0]
+                if pt[1] < min_y:
+                        min_y = pt[1]
+                if pt[0] > max_x:
+                        max_x = pt[0]
+                if pt[1] > max_y:       
+                        max_y = pt[1]
+
+        for index in range(len(pts)):
+                pts[index][0] = pts[index][0] - min_x
+                pts[index][1] = pts[index][1] - min_y
+                pts[index] = tuple(pts[index])
+
+        max_x = max_x - min_x
+        max_y = max_y - min_y
+
+        #print("max_x: {0}, max_y {1}".format(max_x, max_y))
+        if max_x <= 1 or max_y <= 1:
+                return
+
+        frame = create_blank(max_x, max_y)
+        # print(pts)
+
         #pts = [(int(pt[0] * 3.0) - 1500, int(pt[1] * 3.0) - 1500) for pt in ps]
         for index in range(len(pts) - 1):
                 cv2.line(frame, pts[index], pts[index + 1], (255, 255, 255))
 
+        cv2.imshow(frameName, frame)
 
-points = []
-points2 = []
+greenPointsTrail = []
+greenPointsWindow = "green"
+orangePointsTrail = []
+orangePointsWindow = "orange"
+
 while True:
         frame = vs.read()[1]
 
@@ -78,15 +126,15 @@ while True:
         greenLocs = findColor(frame, greenLower, greenUpper)
         greenPoints = getPts(greenLocs)
         drawColorLocations(frame, greenLocs)
-        points2 += greenPoints
+        greenPointsTrail += greenPoints
 
         orangeLocs = findColor(frame, orangeLower, orangeUpper)
         orangePoints = getPts(orangeLocs)
         drawColorLocations(frame, orangeLocs)
-        points += orangePoints
+        orangePointsTrail += orangePoints
 
-        drawPath(frame, points2)
-        drawPath(frame, points)
+        drawPath(greenPointsWindow, greenPointsTrail)
+        drawPath(orangePointsWindow, orangePointsTrail)
  
         # show the frame to our screen
         cv2.imshow("Frame", frame)
@@ -96,7 +144,7 @@ while True:
         if key == ord("q"):
                 break
 
- 
+
 vs.release()
  
 # close all windows
