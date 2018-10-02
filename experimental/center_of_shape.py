cab5139e585a71540000a2861be9a0e5a1462511
Brian Cui <brian.cui@live.com>
2018-10-02 14:34:30 -0700

    add motion track experiment

---
+++ experimental/center_of_shape.py
@@ -0,0 +1,20 @@
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+    help="path to the input image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, blur it slightly,
+# and threshold it
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY)[1]
+
+cv2.imshow("threshold", thresh)
+cv2.waitKey(0)
