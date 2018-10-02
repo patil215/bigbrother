9196dc076a744b2d8f4940ec9e4a033093694529
Brian Cui <brian.cui@live.com>
2018-10-02 14:51:07 -0700

    add slider bars to edge detect

--- experimental/edge_detect.py
+++ experimental/edge_detect.py
@@ -1,8 +1,32 @@
 import cv2
 import numpy as np
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True)
+args = vars(ap.parse_args())
+
 # https://docs.opencv.org/3.1.0/da/d22/tutorial_py_canny.html
-img = cv2.imread("./hello.jpg")
+img = cv2.imread(args["image"])
 edges = cv2.Canny(img, 0, 150)
 print(edges)
+
+callback = lambda x : None
+
+cv2.namedWindow("edges")
+
+cv2.createTrackbar("min", "edges", 0, 500, callback)
+cv2.createTrackbar("max", "edges", 150, 500, callback)
 cv2.imshow("edges", edges)
-cv2.waitKey(0)
+
+while True:
+    edges = cv2.Canny(img,
+        cv2.getTrackbarPos("min", "edges"),
+        cv2.getTrackbarPos("max", "edges"))
+    
+    cv2.imshow("edges", edges)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:
+        break
+
