e4675224a3c448c7ea8e8400ce9708f41ece5036
Neil Patil <neilpatil215@gmail.com>
2018-11-07 01:34:50 -0600

    Rewrite segmenting to use BFS pruning

--- projection-method/classify.py
+++ projection-method/classify.py
@@ -6,13 +6,83 @@ import operator
 from tracepoint import TracePath
 import math
 
+def generate_intervals(start_ind,
+		path_length,
+		fps,
+		step_start_millis,
+		step_end_millis,
+		step_size_millis,
+		space_start_millis,
+		space_end_millis,
+		space_step_millis
+	):
+	MILLIS_PER_FRAME = 1000.0 / fps
+
+	intervals = []
+
+	for start_frame_index in range(start_ind + int(space_start_millis / MILLIS_PER_FRAME), start_ind + int(space_end_millis / MILLIS_PER_FRAME), int(space_step_millis / MILLIS_PER_FRAME)):
+		interval_start_frame = start_frame_index + int(step_start_millis / MILLIS_PER_FRAME)
+		interval_end_frame = start_frame_index + int(step_end_millis / MILLIS_PER_FRAME)
+		frames_per_step = int(step_size_millis / MILLIS_PER_FRAME)
+
+		for end_frame_index in range(interval_start_frame, interval_end_frame, frames_per_step):
+			if end_frame_index >= path_length:
+				break  # Don't include segments going out of bounds
+			intervals.append((start_frame_index, end_frame_index))
+
+	return intervals
+
+def print_classifications(classification_lists):
+	print("Candidates:")
+	for classification_list in classification_lists:
+		total_score = classification_list[-1][0]
+		classes = [x[1] for x in classification_list[1:]]
+		times = [str((x[2], x[3])) for x in classification_list[1:]]
+		print("{}: Times: {} (Score: {})".format("->".join(classes), ",".join(times), total_score))
+	print()
+
+def bfs_segment(tracepath, candidates, num_digits, fps, 
+		K=10,
+		STEP_START_MILLIS=400,
+		STEP_END_MILLIS=1000,
+		STEP_SIZE_MILLIS=50,
+		SPACE_START_MILLIS=500,
+		SPACE_END_MILLIS=1000,
+		SPACE_STEP_MILLIS=100
+	):
+	# Format: List[(total_score, class_name, frame_start, frame_end)]
+	current_classifications = [[(0.0, "start", 0, 0)]]
+	num_digits_sequenced = 0
+	MILLIS_PER_FRAME = 1000.0 / fps
+
+	while num_digits_sequenced < num_digits:
+		new_classifications = []
+		for classification_list in current_classifications:
+			latest_score, latest_class, latest_frame_start_index, latest_frame_end_index = classification_list[-1]
+
+			new_intervals = generate_intervals(latest_frame_end_index, len(tracepath.path), fps, STEP_START_MILLIS, STEP_END_MILLIS, STEP_SIZE_MILLIS, SPACE_START_MILLIS, SPACE_END_MILLIS, SPACE_STEP_MILLIS)
+			for start_index, end_index in new_intervals:
+				path_slice = TracePath(path=tracepath.path[start_index:end_index + 1])
+				path_slice.normalize()
+				result, distance = classifyDTW(candidates, path_slice)[0]
+
+				new_classification_list = classification_list + [(latest_score + distance, result, start_index, end_index)]
+				new_classifications.append(new_classification_list)
+
+		# Take the top K
+		current_classifications = sorted(new_classifications, key=lambda classification_list: classification_list[-1])[:K]
+		print_classifications(current_classifications)
+		num_digits_sequenced += 1
+
+	return current_classifications
+
 def prep_data(data, R):
 	for category in data:
 		for tracepath in data[category]:
 			tracepath.transform(R)
 			tracepath.normalize()
 
-def recursive_segment(tracepath, candidates, num_digits_left, current_path_index=0, STEP_DURATION=500, FPS=29.97):
+def recursive_segment(tracepath, candidates, num_digits_left, current_path_index=0, STEP_DURATION=50, FPS=29.97):
 	if num_digits_left == 0:
 		return [(0, [])]
 
