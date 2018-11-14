de5f4abe3f8110575230a41a675c5c53b91c5e02
Brian Cui <brian.cui@live.com>
2018-11-13 16:10:00 -0800

    add confusion matrix

--- projection-method/predict_batch.py
+++ projection-method/predict_batch.py
@@ -1,28 +1,68 @@
+import itertools
 import math
 import os
 import time
-from time import sleep
 from collections import defaultdict
+from time import sleep
 
 import click
 import cv2
 import imutils
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
 from tabulate import tabulate
 
-from classify import classifyDTW, prep_data, bfs_segment, get_class_time_ranges
-from fileutils import read_obj, write_obj, read_training_data, get_test_segment_tree, get_test_path_tree
+from classify import bfs_segment, classifyDTW, get_class_time_ranges, prep_data
+from fileutils import (get_test_path_tree, get_test_segment_tree, read_obj,
+                       read_training_data, write_obj)
 from motiontrack import Tracker
 from project import eulerAnglesToRotationMatrix
 from readvideo import tracepath_from_frames
 from tracepoint import TracePath, TracePoint
 from vizutils import draw_tracepoints, plotPath
 
+
+def plot_confusion_matrix(cm, classes,
+						  normalize=False,
+						  title='Confusion matrix',
+						  cmap=plt.cm.Blues):
+	"""
+	This function prints and plots the confusion matrix.
+	Normalization can be applied by setting `normalize=True`.
+	"""
+	if normalize:
+		cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+	plt.imshow(cm, interpolation='nearest', cmap=cmap)
+	plt.title(title)
+	plt.colorbar()
+	tick_marks = np.arange(len(classes))
+	plt.xticks(tick_marks, classes, rotation=45)
+	plt.yticks(tick_marks, classes)
+
+	fmt = '.2f' if normalize else 'd'
+	thresh = cm.max() / 2.
+	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+		plt.text(j, i, format(cm[i, j], fmt),
+				 horizontalalignment="center",
+				 color="white" if cm[i, j] > thresh else "black")
+
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+	plt.tight_layout()
+
+
 def update_statistics(statistics, classifications, video_class):
 	top3 = [thing[0] for thing in classifications[:3]]
 	top5 = [thing[0] for thing in classifications[:5]]
-	print("Predicted: {}, Expected: {}".format(classifications[0][0], video_class))
+	print("Predicted: {}, Expected: {}".format(
+		classifications[0][0], video_class))
+
+	if statistics[video_class]["classification"] == 0:
+		statistics[video_class]["classification"] = defaultdict(int)
+	statistics[video_class]["classification"][classifications[0][0]] += 1
+
 	if classifications[0][0] == video_class:
 		statistics[video_class]["correct"] += 1
 	if video_class in top3:
@@ -30,6 +70,7 @@ def update_statistics(statistics, classifications, video_class):
 	if video_class in top5:
 		statistics[video_class]["correct_top5"] += 1
 	statistics[video_class]["total"] += 1
+
 
 def print_statistics(statistics):
 	print("SUMMARY OF RESULTS:")
@@ -43,10 +84,38 @@ def print_statistics(statistics):
 	print("BY CLASS:")
 	print(
 		tabulate(
-			[[c, statistics[c]["correct"]/statistics[c]["total"], statistics[c]["correct_top3"], statistics[c]["correct_top5"], statistics[c]["total"]] for c in statistics],
+			[[c, statistics[c]["correct"]/statistics[c]["total"], statistics[c]["correct_top3"],
+				statistics[c]["correct_top5"], statistics[c]["total"]] for c in statistics],
 			headers=["Class", "Percent correct", "Top 3", "Top 5", "Total"]
 		)
 	)
+
+	class_names = list(statistics.keys())
+	pred = []
+	truth = []
+
+	# generate samples for the confusion matrix
+	for truth_class in class_names:
+		for prediction_class in statistics[truth_class]["classification"]:
+			for i in range(statistics[truth_class]["classification"][prediction_class]):
+				pred.append(prediction_class)
+				truth.append(truth_class)
+
+	cnf_matrix = confusion_matrix(truth, pred, labels=class_names)
+	np.set_printoptions(precision=2)
+
+	# Plot non-normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names,
+						title='Confusion matrix, without normalization')
+
+	# Plot normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+						title='Normalized confusion matrix')
+
+	plt.show()
+
 
 def do_prediction(training_data, path, sequence_length, statistics, video_class):
 	if sequence_length == 1:
@@ -84,12 +153,14 @@ def predict(test_dir, data, angle, length):
 	for video_class in path_tree:
 		class_paths = path_tree[video_class]
 		for path_name in class_paths:
-			path = read_obj("{}/{}/{}".format(test_dir, video_class, path_name))
+			path = read_obj(
+				"{}/{}/{}".format(test_dir, video_class, path_name))
 			path.normalize()
 
 			do_prediction(training_data, path, length, statistics, video_class)
 
 	print_statistics(statistics)
+
 
 if __name__ == "__main__":
 	predict()
