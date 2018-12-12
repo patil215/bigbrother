f3fc258fe0822b51fde56c0fcac354b5d473c039
Brian Cui <brian.cui@live.com>
2018-12-11 16:41:48 -0800

    extend csv output to folder wide

--- projection-method/print_stats.py
+++ projection-method/print_stats.py
@@ -1,24 +1,35 @@
 import click
 import sys
+import os
 
 from fileutils import read_obj
 
-@click.command()
-@click.argument('filename')
-def print_stats(filename):
+def write_csv(filename):
+	print(filename)
 	input = read_obj(filename)
 	if input is None:
 		print("The specified file does not exist.")
 		sys.exit(1)
 
 	sequence_length = int(filename.split("/")[-1].split("_")[0])
-	print("Max Rank,Count")
+	output_filename = "{}{}_digits.csv".format(filename[:filename.rfind('/') + 1], sequence_length)
+	output_file = open(output_filename, 'w')
+	print("Max Rank,Count", file=output_file)
 
 	for i in range(1, 11):
-		print("{},{}".format(i, input[str(i)]))
+		print("{},{}".format(i, input[str(i)]), file=output_file)
 
-	print("Total,{}".format(input["total"]))
-	print("Sequence Length,{}".format(sequence_length))
+	print("Total,{}".format(input["total"]), file=output_file)
+	print("Sequence Length,{}".format(sequence_length), file=output_file)
+
+@click.command()
+@click.argument('folder')
+def print_stats(folder):
+	subfiles = ["{}/{}".format(folder, f.name) for f in os.scandir(folder)
+		if not f.is_dir() and "statistics" in f.name]
+
+	for stats_file in subfiles:
+		write_csv(stats_file)
 
 if __name__ == "__main__":
 	print_stats()
