46eb25f8d9e71e45317ef8295f143922035222ca
Brian Cui <brian.cui@live.com>
2018-12-11 16:32:37 -0800

    print csv

--- projection-method/print_stats.py
+++ projection-method/print_stats.py
@@ -11,7 +11,14 @@ def print_stats(filename):
 		print("The specified file does not exist.")
 		sys.exit(1)
 
-	print(input)
+	sequence_length = int(filename.split("/")[-1].split("_")[0])
+	print("Max Rank,Count")
+
+	for i in range(1, 11):
+		print("{},{}".format(i, input[str(i)]))
+
+	print("Total,{}".format(input["total"]))
+	print("Sequence Length,{}".format(sequence_length))
 
 if __name__ == "__main__":
 	print_stats()
